347827f80fd76a8bf9746f769bbb3b4f69829d4b
Sampiquepas <92890152+Sampiquepas@users.noreply.github.com>
2021-10-26 10:08:18 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    personne += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    personne = 0
+})
+input.onButtonPressed(Button.B, function () {
+    personne += -1
+})
+let personne = 0
+personne = 0
+basic.forever(function () {
+    basic.showNumber(personne)
+    if (personne == 20) {
+        basic.showString("plein")
+    }
+})
